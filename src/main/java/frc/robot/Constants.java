80251512cb061b0f83b1d87dc4fd47251c1ffa42
3487 Red Pride Robotics <team3487@gmail.com>
2024-01-18 16:26:32 -0500

    yay
    it works now

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,14 +6,17 @@ public class Constants {
         public static final int right_Drive_ID = 1;
     }
     public static class Intake{
-        public static final int intake_Motor_ID = 2;
-        public static double IntakeSpeed = 0.8;
-        public static double ShootSpeed = 0.8;
+        public static final int intake_Motor_ID_Top = 2;
+        public static final int intake_Motor_ID_Bottom = 3;
+        public static double IntakeSpeed = 1;
+        public static double ShootSpeed = 1;
+        public static final double waitingNumber = 1;
     }
 
     public static class DriveEdits{
         public static boolean left_Drive_Inverse = true;
         public static boolean right_Drive_Inverse = false;
-		public static double DriveSpeed = 0.8;
+		public static double DriveSpeed = 1;
     }
 }
+//this is a test
